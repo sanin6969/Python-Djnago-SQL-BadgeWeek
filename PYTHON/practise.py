0219244c31f3015c808a67b52fdb61af345a841f
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-06-08 12:27:42 +0530

    python

---
+++ PYTHON/practise.py
@@ -0,0 +1,22 @@
+# end parameters
+# print('heloo' ,end =' ')
+# print('world')
+
+# sep parameters
+# print('15','04','2003',sep='-')
+
+#                                                       TODO: DATATYPES
+
+#           TODO NUMERIC
+
+# INTEGERS
+# a=10
+# b=-29
+# c=6576565675675
+# print(type(a),type(b),type(c))
+
+# FLOAT
+# a=109988.21
+# b=-2567.776507786
+# print(type(a),type(b))
+
