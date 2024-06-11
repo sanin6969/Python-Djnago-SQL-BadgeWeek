ffe40ca8f82cad255b49e898c64d0c737a33efa3
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-06-11 12:24:03 +0530

    py

--- PYTHON/practise.py
+++ PYTHON/practise.py
@@ -20,3 +20,61 @@
 # b=-2567.776507786
 # print(type(a),type(b))
 
+#                   TODO sequence
+
+# STRINgs slicing
+# a='sanin'
+# print(a[1:3])
+# print(a[0:-4])
+# print(a[::-2])
+
+# list comprehension
+# total=sum([x for x in range(1,10) if x%2==0])
+# print(total)
+
+# a=1,2,3,4,55,
+# # print(type(a))
+
+# a=((1,2),(3,4))
+# print(a[0:2])
+
+# Dict={}
+# Dict = dict([(4, 'Rinku'), (2, 'Singh')]) 
+# print('name:',Dict[4])
+# print(Dict)
+
+
+# def weekday():
+#     day=int(input('enter a number between 1-7: '))
+    
+#     match day:
+#         case 1:
+#             print('MONDAY')
+#         case 2:
+#             print('TUESDAY')
+#         case 3:
+#             print('WEDNESDAY')
+#         case 4:
+#             print('THURSDAY')
+#         case 5:
+#             print('FRIDAY')
+#         case 6:
+#             print('SATURDAY')
+#         case 7:
+#             print('SUNDAY')
+#         case _:
+#             print('enter number between 1-7')
+            
+# weekday()
+    
+# def outerFunction(text): 
+ 
+#     def innerFunction(text): 
+#         print(text) 
+ 
+#     # Note we are returning function
+#     # WITHOUT parenthesis
+#     return innerFunction  
+# outerFunction('hey')
+
+
